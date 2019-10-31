6679e0945aa03c6e849769662dbaf0e8008ade6f
Justin Lee <julee@redhat.com>
2019-10-31 15:33:15 -0400

    update lambda to use the new module

--- amazon-lambda/src/main/java/com/antwerkz/quarkus/lambda/HelloApp.java
+++
@@ -1,8 +0,0 @@
-package com.antwerkz.quarkus.lambda;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/hello")
-public class HelloApp extends Application {
-}
